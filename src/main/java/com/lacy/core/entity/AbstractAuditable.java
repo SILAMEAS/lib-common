efa34b9eb7ddfcb6d5379ea0e4f0962301e738dd
Sila MEAS <sila.meas@allweb.com.kh>
2026-03-13 12:43:21 +0700

    lib common

--- src/main/java/com/lacy/core/entity/AbstractAuditable.java
+++ src/main/java/com/lacy/core/entity/AbstractAuditable.java
@@ -3,18 +3,14 @@ package com.lacy.core.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.metamodel.StaticMetamodel;
-import java.time.Instant;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -24,41 +20,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners({AuditingEntityListener.class})
 @StaticMetamodel(AbstractAuditable.class)
-public abstract class AbstractAuditable implements PreOperationInterface {
-
-  @CreatedDate
-  @Column(name = "created_at", updatable = false, nullable = false)
-  private Instant createdAt;
-
-  @LastModifiedDate
-  @Column(name = "updated_at", nullable = false)
-  private Instant updatedAt;
+public abstract class AbstractAuditable extends AbstractTimestamp {
 
   @CreatedBy
   @Column(name = "created_by", updatable = false)
-  private Long createdBy;
+  private UUID createdBy;
 
   @LastModifiedBy
   @Column(name = "updated_by")
-  private Long updatedBy;
-
-  @PrePersist
-  public void onCreated() {
-    this.onPreCreated();
-  }
-
-  @PreUpdate
-  public void onUpdated() {
-    this.onPreUpdated();
-  }
-
-  @Override
-  public void onPreCreated() {
-    /* On created*/
-  }
-
-  @Override
-  public void onPreUpdated() {
-    /* On updated*/
-  }
+  private UUID updatedBy;
 }
---
+++ src/main/java/com/lacy/core/entity/AbstractTimestamp.java
@@ -0,0 +1,54 @@
+package com.lacy.core.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.metamodel.StaticMetamodel;
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class})
+@StaticMetamodel(AbstractTimestamp.class)
+public abstract class AbstractTimestamp implements PreOperationInterface {
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false, nullable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
+
+  @PrePersist
+  public void onCreated() {
+    this.onPreCreated();
+  }
+
+  @PreUpdate
+  public void onUpdated() {
+    this.onPreUpdated();
+  }
+
+  @Override
+  public void onPreCreated() {
+    /* On created*/
+  }
+
+  @Override
+  public void onPreUpdated() {
+    /* On updated*/
+  }
+}
